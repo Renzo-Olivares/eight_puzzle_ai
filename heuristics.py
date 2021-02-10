5e13ee3d377608f79e793aec9170c9d24372f363
Renzo <rmolivares@renzo-olivares.dev>
2021-02-10 01:41:06 -0800

    Implement misplaced tiles heuristic and simple text UI

--- heuristics.py
+++ heuristics.py
@@ -5,8 +5,43 @@ class Heuristics:
         
     def h1(self, state):
         # Misplaced Tile Heuristic: The number of misplaced tiles (blank not included).
-        pass
+        misplacedCount = 0
+        goal = self.buildGoal(state)
+
+        # Compare goal state and given state.
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                if state[i][j] != goal[i][j] and state[i][j] != 0:
+                    misplacedCount += 1
+
+        return misplacedCount
 
     def h2(self, state):
         # Manhattan Distance Heuristic: The sum of the distances of the tiles from their goal positions.
-        pass
+        goal = self.buildGoal(state)
+
+        # Compare goal state and given state.
+        # for i in range(len(state)):
+        #     for j in range(len(state[i])):
+        #         if state[i][j] != goal[i][j] and state[i][j] != 0:
+        pass     
+
+    def buildGoal(self, state):
+        # Build goal for comparison.
+        goal = []
+        counter = 1
+        temp = [] 
+
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                if j == len(state[i]) - 1 and i == len(state) - 1:
+                    temp.append(0)
+                else:
+                    temp.append(counter)
+                counter += 1
+                if j == len(state[i]) - 1:
+                    copy = temp[:]
+                    goal.append(copy)
+                    temp.clear()
+        
+        return goal
--- main.py
+++ main.py
@@ -12,10 +12,44 @@ depthTwenty = [[7,1,2], [4,8,5], [6,3,0]]
 depthTwentyFour = [[0,7,2], [4,6,1], [3,5,8]]
 
 def main():
+    # Setup
     solver = Solver()
-    problem = Problem(depthSixteen)
+    problem = None
     heuristics = Heuristics()
-    print((solver.generalSearch(problem, solver.bestFirstSearchQueueingFunction, heuristics.h0)).state)
+    selectedBoard = None
+
+    # Text line UI
+    print("Welcome to Renzo's 8-puzzle solver.")
+    print('Type "1" to use a default puzzle, or "2" to enter your own puzzle.')
+    puzzleSelection = input('')
+
+    if puzzleSelection == '2':
+        print('\n\t Enter your puzzle, use a zero to represent the blank')
+        row1 = input('\t Enter the first row, use space or tabs between numbers      ').split()
+        row2 = input('\t Enter the second row, use space or tabs between numbers     ').split()
+        row3 = input('\t Enter the third row, use space or tabs between numbers      ').split()
+        row1 = [int(i) for i in row1]
+        row2 = [int(i) for i in row2]
+        row3 = [int(i) for i in row3]
+        selectedBoard = [row1, row2, row3]
+    else:
+        selectedBoard = depthZero
+
+    problem = Problem(selectedBoard)
+
+    print('\n \t Enter your choice of algorithm')
+    print('\t 1. Uniform Cost Search')
+    print('\t 2. A* with the Misplaced Tile Heuristic')
+    print('\t 3. A* with the Manhattan Distance Heuristic')
+    algorithmSelection = input('\n\t ')
+
+    if algorithmSelection == '1':
+        print(solver.generalSearch(problem, solver.bestFirstSearchQueueingFunction, heuristics.h0).parent.state)
+    elif algorithmSelection == '2':
+        print(solver.generalSearch(problem, solver.bestFirstSearchQueueingFunction, heuristics.h1).parent.state)
+    elif algorithmSelection == '3':
+        # solver.generalSearch(problem, solver.bestFirstSearchQueueingFunction, heuristics.h2)
+        pass
 
 if __name__ == "__main__":
     main()
